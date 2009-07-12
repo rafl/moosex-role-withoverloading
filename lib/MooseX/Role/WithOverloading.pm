45b64a3984057063301a3330a98c0900d60c2273
Florian Ragwitz <rafl@debian.org>
2009-07-13 00:28:20 +0200

    Add metaclass role.

--- lib/MooseX/Role/WithOverloading.pm
+++ lib/MooseX/Role/WithOverloading.pm
@@ -3,12 +3,19 @@ package MooseX::Role::WithOverloading;
 use Moose::Role ();
 use Moose::Exporter;
 use Moose::Util::MetaRole;
+use MooseX::Role::WithOverloading::Meta::Role;
 
 Moose::Exporter->setup_import_methods(also => 'Moose::Role');
 
 sub init_meta {
     my ($class, %opts) = @_;
     my $meta = Moose::Role->init_meta(%opts);
+
+    Moose::Util::MetaRole::apply_metaclass_roles(
+        for_class       => $opts{for_class},
+        metaclass_roles => ['MooseX::Role::WithOverloading::Meta::Role'],
+    );
+
     return $meta;
 }
 
---
+++ lib/MooseX/Role/WithOverloading/Meta/Role.pm
@@ -0,0 +1,6 @@
+package MooseX::Role::WithOverloading::Meta::Role;
+
+use Moose::Role;
+use namespace::autoclean;
+
+1;
