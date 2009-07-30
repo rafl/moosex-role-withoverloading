a4e33ca171dfadf37ee09b4fc92ac608cffe6379
Florian Ragwitz <rafl@debian.org>
2009-07-30 16:12:58 +0200

    Apply special application roles to the generated composite role.

--- lib/MooseX/Role/WithOverloading/Meta/Role/Composite.pm
+++ lib/MooseX/Role/WithOverloading/Meta/Role/Composite.pm
@@ -2,15 +2,20 @@ package MooseX::Role::WithOverloading::Meta::Role::Composite;
 
 use Moose::Role;
 use Moose::Util::MetaRole;
-use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::RoleSummation';
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::Composite::ToClass';
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::Composite::ToRole';
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::Composite::ToInstance';
+
 
 use namespace::clean -except => 'meta';
 
-around _new => sub {
+around apply_params => sub {
     my ($next, $self, @args) = @_;
     return Moose::Util::MetaRole::apply_metaclass_roles(
-        for                                    => $self->$next(@args),
-        application_role_summation_class_roles => [ RoleSummation ],
+        for                                 => $self->$next(@args),
+        application_to_class_class_roles    => [ ToClass    ],
+        application_to_role_class_roles     => [ ToRole     ],
+        application_to_instance_class_roles => [ ToInstance ],
     );
 };
 
