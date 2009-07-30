9fd1e266d3d6d6ac803990606b674198b8f30458
Florian Ragwitz <rafl@debian.org>
2009-07-30 10:54:37 +0200

    Add a ToInstance application role.

---
+++ lib/MooseX/Role/WithOverloading/Meta/Role/Application/ToInstance.pm
@@ -0,0 +1,8 @@
+package MooseX::Role::WithOverloading::Meta::Role::Application::ToInstance;
+
+use Moose::Role;
+use namespace::autoclean;
+
+with 'MooseX::Role::WithOverloading::Meta::Role::Application';
+
+1;
