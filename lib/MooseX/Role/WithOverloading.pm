a7be30539b436322ccb9086b3d0cf02ec95e0ee8
Florian Ragwitz <rafl@debian.org>
2009-07-30 14:40:11 +0200

    Apply our meta role, so we get called back on rolesummation applications.

--- lib/MooseX/Role/WithOverloading.pm
+++ lib/MooseX/Role/WithOverloading.pm
@@ -3,6 +3,7 @@ package MooseX::Role::WithOverloading;
 use Moose::Role ();
 use Moose::Exporter;
 use Moose::Util::MetaRole;
+use aliased 'MooseX::Role::WithOverloading::Meta::Role', 'MetaRole';
 use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToClass';
 use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToRole';
 use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToInstance';
@@ -15,14 +16,13 @@ sub init_meta {
     my ($class, %opts) = @_;
     my $meta = Moose::Role->init_meta(%opts);
 
-    Moose::Util::MetaRole::apply_metaclass_roles(
+    return Moose::Util::MetaRole::apply_metaclass_roles(
         for_class                           => $opts{for_class},
+        metaclass_roles                     => [ MetaRole   ],
         application_to_class_class_roles    => [ ToClass    ],
         application_to_role_class_roles     => [ ToRole     ],
         application_to_instance_class_roles => [ ToInstance ],
     );
-
-    return $meta;
 }
 
 1;
