9093bd27867f1a995e47be56f3517fe298390e9d
Florian Ragwitz <rafl@debian.org>
2009-07-30 10:55:08 +0200

    Add a ToRole application role.

---
+++ lib/MooseX/Role/WithOverloading/Meta/Role/Application/ToRole.pm
@@ -0,0 +1,21 @@
+package MooseX::Role::WithOverloading::Meta::Role::Application::ToRole;
+
+use Moose::Role;
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToClass';
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToInstance';
+use namespace::autoclean;
+
+with 'MooseX::Role::WithOverloading::Meta::Role::Application';
+
+around apply => sub {
+    my ($next, $self, $role1, $role2) = @_;
+    $role2 = Moose::Util::MetaRole::apply_metaclass_roles(
+        for_class                           => $role2->name,
+        application_to_class_class_roles    => [ ToClass     ],
+        application_to_role_class_roles     => [ __PACKAGE__ ],
+        application_to_instance_class_roles => [ ToInstance  ],
+    );
+    return $self->$next($role1, $role2);
+};
+
+1;
