58608736cc165a33730c1640b3b999de49065b6a
Florian Ragwitz <rafl@debian.org>
2009-07-30 10:55:26 +0200

    Apply all role application class roles.

--- lib/MooseX/Role/WithOverloading.pm
+++ lib/MooseX/Role/WithOverloading.pm
@@ -3,7 +3,11 @@ package MooseX::Role::WithOverloading;
 use Moose::Role ();
 use Moose::Exporter;
 use Moose::Util::MetaRole;
-use MooseX::Role::WithOverloading::Meta::Role::Application::ToClass;
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToClass';
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToRole';
+use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::ToInstance';
+
+use namespace::clean;
 
 Moose::Exporter->setup_import_methods(also => 'Moose::Role');
 
@@ -12,8 +16,10 @@ sub init_meta {
     my $meta = Moose::Role->init_meta(%opts);
 
     Moose::Util::MetaRole::apply_metaclass_roles(
-        for_class                        => $opts{for_class},
-        application_to_class_class_roles => ['MooseX::Role::WithOverloading::Meta::Role::Application::ToClass'],
+        for_class                           => $opts{for_class},
+        application_to_class_class_roles    => [ ToClass    ],
+        application_to_role_class_roles     => [ ToRole     ],
+        application_to_instance_class_roles => [ ToInstance ],
     );
 
     return $meta;
