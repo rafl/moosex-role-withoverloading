8733a281432051c783bc35127aa45a4a305f79ec
Florian Ragwitz <rafl@debian.org>
2009-07-31 07:51:49 +0200

    Try clarifying some of the magic bits.

--- lib/MooseX/Role/WithOverloading/Meta/Role/Application.pm
+++ lib/MooseX/Role/WithOverloading/Meta/Role/Application.pm
@@ -24,9 +24,12 @@ sub apply_overloading {
     my ($self, $role, $other) = @_;
     return unless overload::Overloaded($role->name);
 
+    # overloading predicate method
     $other->add_package_symbol('&()' => $role->get_package_symbol('&()'));
+    # fallback value
     $other->add_package_symbol('$()' => $role->get_package_symbol('$()'))
         if $role->has_package_symbol('$()');
+    # register with magic by touching
     $other->get_package_symbol('%OVERLOAD')->{dummy}++;
 
     for my $op (@{ $self->overload_ops }) {
@@ -39,6 +42,7 @@ sub apply_overloading {
         next unless $meth;
 
         if ($meth == \&overload::nil) {
+            # use overload $op => 'method_name';
             my $scalar_sym = qq{\$($op};
             $other->add_package_symbol($code_sym => $meth);
             $other->add_package_symbol(
@@ -46,6 +50,7 @@ sub apply_overloading {
             );
         }
         else {
+            # use overload $op => sub { ... };
             $other->add_package_symbol(qq{&($op} => $meth);
         }
     }
