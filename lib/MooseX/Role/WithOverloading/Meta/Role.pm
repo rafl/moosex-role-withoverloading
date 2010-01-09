b72e3050a87e055f774f29a859cbe32caa7b5e98
Tomas Doran <bobtfish@bobtfish.net>
2010-01-09 18:13:24 +0000

    Work correctlywith new Moose versions

--- lib/MooseX/Role/WithOverloading/Meta/Role.pm
+++ lib/MooseX/Role/WithOverloading/Meta/Role.pm
@@ -1,12 +1,19 @@
 package MooseX::Role::WithOverloading::Meta::Role;
 # ABSTRACT: Roles which support overloading
 
+use Moose ();
 use Moose::Role;
 use aliased 'MooseX::Role::WithOverloading::Meta::Role::Composite', 'CompositionRole';
 use namespace::autoclean;
 
-has '+composition_class_roles' => (
-    default => [ CompositionRole ],
-);
+$Moose::VERSION >= 0.9301
+    ? around composition_class_roles => sub {
+        my ($orig, $self) = @_;
+        return $self->$orig,
+            CompositionRole;
+    }
+    : has '+composition_class_roles' => (
+        default => sub { [ CompositionRole ] },
+    );
 
 1;
