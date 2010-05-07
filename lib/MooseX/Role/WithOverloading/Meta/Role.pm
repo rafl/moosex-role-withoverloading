ce36197c6583c3612a3e24ebfbccaa32bfdc7ad3
Florian Ragwitz <rafl@debian.org>
2010-05-07 18:59:24 +0200

    Disable workaround for old Moose versions.

--- lib/MooseX/Role/WithOverloading/Meta/Role.pm
+++ lib/MooseX/Role/WithOverloading/Meta/Role.pm
@@ -1,19 +1,14 @@
 package MooseX::Role::WithOverloading::Meta::Role;
 # ABSTRACT: Roles which support overloading
 
-use Moose ();
+use Moose 0.94 ();
 use Moose::Role;
 use aliased 'MooseX::Role::WithOverloading::Meta::Role::Composite', 'CompositionRole';
 use namespace::autoclean;
 
-$Moose::VERSION >= 0.9301
-    ? around composition_class_roles => sub {
-        my ($orig, $self) = @_;
-        return $self->$orig,
-            CompositionRole;
-    }
-    : has '+composition_class_roles' => (
-        default => sub { [ CompositionRole ] },
-    );
+around composition_class_roles => sub {
+    my ($orig, $self) = @_;
+    return $self->$orig, CompositionRole;
+};
 
 1;
